bc047be8894e5ffc87746adaeaf814da481331e3
Kruti Patel <krutipatel@krutis-mbp.lan>
2023-07-03 00:06:35 -0400

    Change_Validation

---
+++ MediFast-Frontend-main/client/src/commonComponents/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import dashboard from '../commonComponentsAssets/dashboard.jpg'
+import './dashboard.css'
+import { Link,useNavigate} from "react-router-dom";
+const Dashboard = () => {
+  return (
+    <div className='common-main'>
+        <img src={dashboard} className='background-image'></img>
+        <div className="common-overlay-button">
+            <button><Link to='/patientLogin'>Patient Login</Link></button>
+            <button><Link to='/doctorLogin'>Doctor Login</Link></button>
+        </div>
+    </div>
+  )
+}
+
+export default Dashboard
